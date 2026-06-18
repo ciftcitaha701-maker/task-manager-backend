587014e92a8389997601d64a76535ce4e4a2e0ba
ciftcitaha701-maker <ciftcitaha701@gmail.com>
2026-06-19 00:23:59 +0200

    POST Endpunkt und Setter hinzugefuegt

--- src/main/java/com/taskmanager/task_manager_backend/TaskController.java
+++ src/main/java/com/taskmanager/task_manager_backend/TaskController.java
@@ -1,22 +1,23 @@
 package com.taskmanager.task_manager_backend;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.Arrays;
+import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "*") // Löst das CORS-Problem
 public class TaskController {
+
+    private List<Task> tasks = new ArrayList<>();
 
     @GetMapping("/tasks")
     public List<Task> getAllTasks() {
-        // Hier geben wir unsere "fiktive" Liste zurück
-        return Arrays.asList(
-            new Task(1, "Vue.js Grundlagen lernen", "2026-05-10"),
-            new Task(2, "Milestone 2 abgeben", "2026-05-15"),
-            new Task(3, "Das Wochenende genießen", "2026-05-09")
-        );
+        return tasks;
+    }
+
+    @PostMapping("/tasks")
+    public Task createTask(@RequestBody Task newTask) {
+        newTask.setId(tasks.size() + 1);
+        tasks.add(newTask);
+        return newTask;
     }
 }
