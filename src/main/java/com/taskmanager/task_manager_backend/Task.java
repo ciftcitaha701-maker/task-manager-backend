fd32eb17a955f8aa638dbdf630d009ff787b7f97
ciftcitaha701-maker <ciftcitaha701@gmail.com>
2026-06-19 09:29:57 +0200

    id von int zu Integer geaendert (JSON null-Fehler behoben)

--- src/main/java/com/taskmanager/task_manager_backend/Task.java
+++ src/main/java/com/taskmanager/task_manager_backend/Task.java
@@ -1,20 +1,20 @@
 package com.taskmanager.task_manager_backend;
 
 public class Task {
-    private int id;
+    private Integer id;
     private String title;
     private String date;
 
     public Task() {}
 
-    public Task(int id, String title, String date) {
+    public Task(Integer id, String title, String date) {
         this.id = id;
         this.title = title;
         this.date = date;
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
 
     public String getTitle() { return title; }
     public void setTitle(String title) { this.title = title; }
